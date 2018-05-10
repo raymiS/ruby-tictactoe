071bc9e7ddf23e570ddac3b6bbef98d14048f137
Raymi Saldomando <raymi@TaylorMcLemores-MacBook-Pro.local>
2018-05-10 17:10:54 -0300

    first commit

---
+++ tictactoe.rb
@@ -0,0 +1,47 @@
+#     ___ 
+#   |_|_|_|
+#   |_|_|_|
+#   |_|_|_|
+#   x = horizontal = rows
+#   y = vertical   = columns  
+
+# [ [ x, x, x ], [ x, x, x ], [ x, x, x ]  ]
+
+# <O> <1>
+tictactoe = [ [  ], [  ], [  ] ]
+
+puts “Choose a letter: X/O”
+player_1 = gets.chomp
+
+if player_1 == “X”
+   player_2 = “O”
+else
+   player_2 = “X”
+end
+
+puts “Player 1: #{player_1.inspect}”
+puts “Player 2: #{player_2.inspect}”
+
+puts “Choose a row: [0-2] ”
+position_x = gets.chomp.to_i
+puts “Choose a column: [0-2] ”
+position_y = gets.chomp.to_i
+
+play = tictactoe[position_x][position_y] = player_1
+
+puts tictactoe.inspect
+
+# 1 
+# right now, assignments are like this: 
+# [[], [], [nil, nil, “O”]]
+# can you do better? seems inconsistent between other of the inner arrays
+
+#2 how to repeat this logic for second player
+
+#3 how to repeat this logic for first player again
+
+#4 how  to win
+
+#5 input validations
+
+#6 cheating validations
